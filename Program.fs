a1d5a7013a83dfae7da13f552a7c103666d7af90
Christian Steinert <chr.steinert@outlook.com>
2019-12-05 09:09:26 +0100

    Initial

---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.IO
+open System.Xml
+open System.Text
+
+let private formatFile (file : FileInfo) =
+    let xml = XmlDocument()
+    xml.Load file.FullName
+    xml.Save file.FullName
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> Array.choose (fun c -> 
+        try
+            let file = c |> FileInfo
+            if file.Exists then file |> Some
+            else None
+        with _ -> None
+    )
+    |> Array.iter formatFile
+    0
